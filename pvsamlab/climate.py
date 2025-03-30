dcabcf69181f7eebbefc389af928df4a9a9909b5
ikoshkin <ihorkoshkin@gmail.com>
2025-03-30 13:30:26 -0600

    Update climate module to use FetchResourceFiles for NSRDB data retrieval and load API credentials from secrets.env

--- pvsamlab/climate.py
+++ pvsamlab/climate.py
@@ -1,78 +1,93 @@
 """
-This module contains functions to work with NSRDB resources
+This module contains functions to work with NSRDB GOES v4 resources using PySAM
 """
-import os
 
+import os
 import pandas as pd
 import numpy as np
 from scipy import spatial
+from dotenv import load_dotenv
+from PySAM.ResourceTools import FetchResourceFiles
 
+# Load secrets
 _dir = os.path.dirname(__file__)
+load_dotenv(os.path.join(_dir, 'secrets.env'))
 
-api_key = 'DEMO_KEY'
-your_name = 'Lolo+Pepe'
-email = 'lolo@pepe.com'
+api_key = os.getenv('NSRDB_API_KEY')
+email = os.getenv('NSRDB_API_EMAIL')
+your_name = os.getenv('NSRDB_API_NAME')
+
 attrs = 'dhi,dni,ghi,air_temperature,surface_pressure,wind_direction,wind_speed'
 
-def download_nsrdb_csv(coords, year='tmy', attributes=attrs):
+
+def download_nsrdb_csv(coords, year='tmy', interval=60):
     """
-    Downloads solar resource file in csv format from NSRDB to a local folder.
+    Downloads NSRDB GOES resource CSV using PySAM's FetchResourceFiles.
 
     Args:
-        lat (float): latitude
-        lon (float): longitude
-        year (str): 'tmy' or '1998' through '2016'
-        attribute (str): list of values to be included
+        coords (tuple): (lat, lon)
+        year (str): 'tmy' or specific year like '2017'
+        interval (int): data interval in minutes (60 or 30)
+
     Returns:
-        wfname (str): path to downloaded file
+        str or None: path to downloaded CSV file
     """
-
     lat, lon = coords
-    wfname = os.path.join(_dir, 'data', 'tmp', f'{coords}_nsrdb_{year}.csv')
-    if os.path.isfile(wfname):
-        return wfname
+    ftag = f'{year}_nsrdb_goes_v4'
+    fname = os.path.join(_dir, 'data', 'tmp', f'{coords}_{ftag}.csv')
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
 
-    url = (f'http://developer.nrel.gov/api/solar/nsrdb_psm3_download.csv?'
-           f'api_key={api_key}&'
-           f'full_name={your_name}'
-           f'&email={email}'
-           f'&affiliation=lolopepe'
-           f'&reason=Prospecting'
-           f'&mailing_list=true'
-           f'&wkt=POINT({lon}+{lat})'
-           f'&names={year}'
-           f'&attributes={attributes}'
-           f'&leap_day=false'
-           f'&utc=false'
-           f'&interval=60')
+    if os.path.isfile(fname):
+        return fname
+
+    resource_type = (
+        'nsrdb-GOES-tmy-v4-0-0' if str(year).lower() == 'tmy'
+        else 'nsrdb-GOES-aggregated-v4-0-0'
+    )
+
     try:
-        wdf = pd.read_csv(url)   
-        wdf.to_csv(wfname, index=False)
-        return wfname
-    except:
-        pass
+        fetcher = FetchResourceFiles(
+            tech='solar',
+            nrel_api_key=api_key,
+            nrel_api_email=email,
+            resource_type=resource_type,
+            resource_year=year,
+            resource_interval_min=interval,
+            resource_dir=os.path.dirname(fname),
+            verbose=False
+        )
+
+        fetcher.fetch([(lon, lat)])
+        if fetcher.resource_file_paths:
+            return fetcher.resource_file_paths[0]
+        else:
+            print("[ERROR] PySAM downloaded, but no file path found.")
+            return None
+
+    except Exception as e:
+        print(f"[ERROR] PySAM download failed: {e}")
+        return None
+
 
 
 def find_nearest(point, set_of_points):
-    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.KDTree.query.html
-
+    """Find index of nearest point from a set using KDTree."""
     tree = spatial.KDTree(set_of_points)
     distance, index_of_nearest = tree.query(point)
     return index_of_nearest
 
 
 def get_ashrae_design_low(lat, lon):
-
+    """Returns the ASHRAE extreme low temperature for the nearest station."""
     df = pd.read_csv(os.path.join(_dir, 'data', '_ashraeDB.csv'))
     stations = df.loc[:, ['Lat', 'Lon']].values
     nearest_station_index = find_nearest((lat, lon), stations)
-    
-
     return df.loc[nearest_station_index, 'ExtrLow']
 
 
 if __name__ == '__main__':
-    # tmy = get_nsrdb_data(41.7566, -111.0480, 'tmy')
-    # print(tmy.head())
-    nf = download_nsrdb_csv((41.7566, -101.0480), '2016')
-    print(nf)
+    tmy = download_nsrdb_csv((30.9759, -97.2465), 'tmy')
+    print(f"TMY file: {tmy}")
+
+    annual = download_nsrdb_csv((30.9759, -97.2465), '2017')
+    print(f"2017 file: {annual}")
